3477af71977d1ff8c1c5ad0f259122c1d148e88c
Harshal-creator <harshalshelar8251@gmail.com>
2023-10-09 16:50:42 +0530

    profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  profileSummary : any = "Immediate joiner. Looking for a better opportunity as an Angular or MEAN STACK  developer With 2 years of relevant Industrial experience in Angular Technology in the IT Industry. Completed B.Tech in Information Technology (6.2 CGPA). And passionate about learning new technologies Expert in creating responsive web applications and have knowledge of backend technologies like Node js."
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openGithub(){
+
+  }
+
+  openLinkedin(){
+    
   }
 
 }
