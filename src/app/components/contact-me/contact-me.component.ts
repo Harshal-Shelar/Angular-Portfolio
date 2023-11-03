e964f6b8a213c438b7898b36433e428b4970af7f
Harshal-creator <harshalshelar8251@gmail.com>
2023-11-03 11:46:59 +0530

    some design changed

--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -12,4 +12,12 @@ export class ContactMeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openMap(){
+    window.open("https://www.google.com/maps/place/16%C2%B056'59.5%22N+74%C2%B024'31.8%22E/@16.9498792,74.4084696,121m/data=!3m1!1e3!4m12!1m7!3m6!1s0x3bc111e397d3afa1:0x87a72d90c7b145a!2sGanapati+Mandir!8m2!3d16.9500258!4d74.4100129!16s%2Fg%2F12hk_z0_1!3m3!8m2!3d16.949867!4d74.408835?entry=ttu");
+  }
+
+  openGmail(){
+    window.open("https://mail.google.com/mail/u/0/#inbox");
+  }
+
 }
